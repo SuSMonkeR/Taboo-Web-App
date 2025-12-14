c40f0c0cb576034f3e45a7f2f2b3728e889c58ff
SuSMonkeR <david_ca94@yahoo.com>
2025-12-13 20:37:23 -0600

    still figuring out render

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://127.0.0.1:8000";
 // If you prefer "http://localhost:8000" and it matches your other API files, that's fine too.
 
 /**
